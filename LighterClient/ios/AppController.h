3b2957526272c171e03b1a037bedef056e4fad62
Enel <spiritring@gmail.com>
2013-05-04 12:54:34 +0800

    Initial Commit

---
+++ LighterClient/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  LighterClientAppController.h
+//  LighterClient
+//
+//  Created by TSEnel on 13-5-4.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ LighterClient/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  LighterClientAppController.h
+//  LighterClient
+//
+//  Created by TSEnel on 13-5-4.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
